b9be6263ce7709062ebd6f2319951f149780c714
Heangleng Taing <taing.steven@gmail.com>
2021-12-13 16:38:43 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- <component :is="Menu"></component>
+    <component :is="Navbar"></component> -->
+    <div>
+      <div>
+        <nuxt />
+      </div>
+    </div>
+    <!-- <component :is="Footer"></component>
+    <component :is="Snackbar"></component> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {};
+  },
+  computed: {
+    // Menu: function() {
+    //   const route_name = this.$route.path;
+    //   const division = route_name.split('/')[1];
+    //   return 'Menu' + division.toUpperCase();
+    // }
+  },
+};
+</script>
+
+<style>
+#app {
+  background-color: #fafafa;
+}
+</style>
